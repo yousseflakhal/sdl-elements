a80fa606abba4175134d3de8ec4f7b1da11f8b00
Youssef LAKHAL <lakhalyoussef1997@gmail.com>
2025-05-21 21:13:19 +0200

    Add GetPosition and GetSize to UIElement

--- src/UIElement.hpp
+++ src/UIElement.hpp
@@ -17,6 +17,8 @@ public:
     virtual void setBounds(int x, int y, int w, int h) { bounds = {x, y, w, h}; }
     void setTheme(const UITheme& theme) { customTheme = theme; hasCustomTheme = true; }
     const UITheme& getTheme() const { return hasCustomTheme ? customTheme : UIConfig::getTheme(); }
+    SDL_Point getPosition() const { return { bounds.x, bounds.y }; }
+    SDL_Point getSize() const { return { bounds.w, bounds.h }; }
 
     virtual ~UIElement() = default;
 
