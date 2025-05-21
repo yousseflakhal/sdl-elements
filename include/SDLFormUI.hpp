04397a4555510403cbb297c4469e19a0f087f5b0
Youssef LAKHAL <lakhalyoussef1997@gmail.com>
2025-05-21 21:10:11 +0200

    Add SetPosition and SetSize Methods to UIElement

--- include/SDLFormUI.hpp
+++ include/SDLFormUI.hpp
@@ -49,6 +49,9 @@ public:
     virtual bool isHovered() const { return false; }
     virtual void update(float dt) = 0;
     virtual void render(SDL_Renderer* renderer) = 0;
+    virtual void setPosition(int x, int y) { bounds.x = x; bounds.y = y; }
+    virtual void setSize(int w, int h) { bounds.w = w; bounds.h = h; }
+    virtual void setBounds(int x, int y, int w, int h) { bounds = {x, y, w, h}; }
     void setTheme(const UITheme& theme) { customTheme = theme; hasCustomTheme = true; }
     const UITheme& getTheme() const { return hasCustomTheme ? customTheme : UIConfig::getTheme(); }
 
