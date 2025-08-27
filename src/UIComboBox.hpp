b349a1417ce49144e0ff5bf7b05fd1471aed5ddd
Youssef LAKHAL <lakhalyoussef1997@gmail.com>
2025-08-27 23:07:12 +0200

    Implement UIComboBox with Default Styling and Focus Effects

--- src/UIComboBox.hpp
+++ src/UIComboBox.hpp
@@ -1,9 +1,14 @@
 #pragma once
 
 #include "UIElement.hpp"
+#include "UIConfig.hpp"
+#include "UIHelpers.hpp"
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL.h>
 #include <vector>
 #include <string>
 #include <functional>
+#include <optional>
 
 class UIComboBox : public UIElement {
 public:
@@ -13,20 +18,30 @@ public:
     void setOnSelect(std::function<void(int)> callback);
     bool isHovered() const override;
     bool isExpanded() const;
-    int getItemCount() const;
-    int getItemHeight() const;
+    int  getItemCount() const;
+    int  getItemHeight() const;
     const SDL_Rect& getBounds() const;
 
     void handleEvent(const SDL_Event& e) override;
     void update(float dt) override;
     void render(SDL_Renderer* renderer) override;
+    UIComboBox* setTextColor(SDL_Color c);
+
+    UIComboBox* setFocusable(bool f) { focusable = f; return this; }
+    bool isFocused() const { return focused; }
 
 private:
     std::vector<std::string> options;
     std::reference_wrapper<int> selectedIndex;
     std::function<void(int)> onSelect;
     TTF_Font* font = nullptr;
+    std::optional<SDL_Color> customTextColor;
+
     bool expanded = false;
-    int hoveredIndex = -1;
+    int  hoveredIndex = -1;
     bool hovered = false;
+
+    bool focused = false;
+    bool focusable = true;
+    int  cornerRadius = 8;
 };
